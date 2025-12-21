4e872e3b62da9e393871f7ed8db38be9baff1c17
Daniël <56074484+kaasiand@users.noreply.github.com>
2025-12-21 22:15:33 +0100

    Add files via upload

--- format.js
+++ format.js
@@ -6,8 +6,9 @@ function formatElement(elemId) {
 function formatLine(str) {
     let output = str;
     let addClass = "";
+    let needClosingUnderscore = false;
     for (let i = 0; i < str.length; i++) {
-        if (str[i] == '[' || str[i] == '{') {
+        if (str[i] == '[' || str[i] == '{' || (!needClosingUnderscore && str[i] == '_')) {
             switch (str[i+1]) {
                 case 'R': addClass = " class='red'"; break;
                 case 'B': addClass = " class='blue'"; break;
@@ -16,10 +17,14 @@ function formatLine(str) {
             }
                  if (str[i] == '[') { output = output.replace(/\[[RBT]?/,    "<b"+addClass+">"); }
             else if (str[i] == '{') { output = output.replace(/\{[RBT]?/, "<span"+addClass+">"); }
+            else if (str[i] == '_') { output = output.replace(/_[RBT]?/,"<small"+addClass+">"); needClosingUnderscore = true; }
+            
         }
         else if (str[i] == ']') { output = output.replace(/\]/,    "</b>"); }
         else if (str[i] == '}') { output = output.replace(/\}/, "</span>"); }
+        else if (str[i] == '_') { output = output.replace(/_/, "</small>"); needClosingUnderscore = false; }
         else if (str[i] == '|') { output = output.replace(/\|/,    "</p><p>"); }
+        else if (str[i] == '¦') { output = output.replace(/\¦/,    "</p><p class='note'>"); }
         else if (str[i] == 'Ø') { output = output.replace(/Ø/,"&NoBreak;"); }
     }
     return output;
